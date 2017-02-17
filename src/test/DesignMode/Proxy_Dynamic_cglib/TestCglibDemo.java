8c3500716558ac0fa8c7392c1fa55b459fcd071e
qinghui <910944453@qq.com>
2017-02-17 16:08:30 +0800

    cglib动态代理，在百度脑图里有阅读springmvc源码笔记

---
+++ src/test/DesignMode/Proxy_Dynamic_cglib/TestCglibDemo.java
@@ -0,0 +1,10 @@
+package test.DesignMode.Proxy_Dynamic_cglib;
+
+public class TestCglibDemo {  
+    
+    public static void main(String[] args) {  
+        BookFacadeCglib cglib=new BookFacadeCglib();  
+        BookFacadeImpl1 bookCglib=(BookFacadeImpl1)cglib.getInstance(new BookFacadeImpl1());  
+        bookCglib.addBook();  
+    }  
+}  
