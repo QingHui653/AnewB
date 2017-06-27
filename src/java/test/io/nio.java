cc5cfed36352ccb18e0110d851cd40250fefbe0e
qinghui <910944453@qq.com>
2017-06-27 18:39:07 +0800

    nio(0)

--- src/java/test/io/nio.java
+++ src/java/test/io/nio.java
@@ -2,19 +2,27 @@ package test.io;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
 import org.junit.Test;
 
+/**
+ * nio 核心     channels 通道  buffers 缓冲   selectors 选择器
+ * @author woshizbh
+ *
+ */
 public class nio {
 	
+	private String relativelyPath =System.getProperty("user.dir");
+	
 	@SuppressWarnings("resource")
-	@Test
-	public void bio1() throws IOException {
-	    String relativelyPath=System.getProperty("user.dir");
+//	@Test
+	public void nio1() throws IOException {
 	    File file= new File(relativelyPath+"\\src\\test\\io\\1.txt");
 	    File file2= new File(relativelyPath+"\\src\\test\\io\\2.txt");
 		FileChannel fni =new FileInputStream(file).getChannel();
@@ -30,6 +38,31 @@ public class nio {
 		fni.close();
 		fno.close();
 		
-		System.out.println(" bio1  nio 写入完成");
+		System.out.println(" nio 写入完成");
+	}
+	
+	@Test
+	public void nio2() throws IOException {
+		RandomAccessFile aFile = new RandomAccessFile(relativelyPath+"\\src\\test\\io\\1.txt", "rw");
+		FileChannel inChannel = aFile.getChannel();
+		//创建48bytes的缓存区
+		ByteBuffer buf = ByteBuffer.allocate(48);
+		//从buff读取数据
+		int bytesRead = inChannel.read(buf);
+		while (bytesRead != -1) {
+			
+			System.out.println("Read " + bytesRead);
+			//make buffer ready for read
+			buf.flip();
+
+			while (buf.hasRemaining()) {
+				System.out.print((char) buf.get()); // read 1 byte at a time
+			}
+			
+			//make buffer ready for writing
+			buf.clear();
+			bytesRead = inChannel.read(buf);
+		}
+		aFile.close();
 	}
 }
