0baf3e101dac6fc2200f19de713476c969106dca
qinghui <910944453@qq.com>
2017-08-05 18:41:38 +0800

    .

--- src/java/test/core/base/StaticTest.java
+++ src/java/test/core/base/StaticTest.java
@@ -2,19 +2,19 @@ package test.core.base;
 
 public class StaticTest {
 	private static String LOCAL="静态变量";
-	
+
 	static{
 		System.out.println("静态代码执行了！");
 	}
-	
+
 	public void sayHello() {
 		System.out.println("hello");
 	}
-	
+
 	public static void main(String[] args) {
 		StaticTest staticTest =new StaticTest();
 		System.out.println(staticTest.LOCAL);
 		staticTest.sayHello();
-		
+
 	}
 }
