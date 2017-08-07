d4186d4b32932dbd53a7e3cce4f5ce47eed412b6
qinghui <910944453@qq.com>
2017-08-07 11:30:35 +0800

    分布式quarz配置

---
+++ src/main/java/newb/c/a_module/jobs/clusterquarz/job/PrintCurrentTimeJobs.java
@@ -0,0 +1,32 @@
+package newb.c.a_module.jobs.clusterquarz.job;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.tools.ant.util.DateUtils;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+import java.util.Date;
+
+/**
+ * @author FengHaixin
+ * @description 一句话描述该文件的用途
+ * @date 2016-05-23
+ */
+public class PrintCurrentTimeJobs extends QuartzJobBean {
+	private static final Log LOG_RECORD = LogFactory.getLog(PrintCurrentTimeJobs.class);
+
+	@Autowired
+    private ClusterQuartz clusterQuartz;
+
+	protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+		LOG_RECORD.info("begin to execute task," + DateUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss:SSS"));
+
+		clusterQuartz.printUserInfo();
+
+		LOG_RECORD.info("end to execute task," + DateUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss:SSS"));
+
+	}
+}
--- src/main/java/newb/c/api/SpringfoxDocConfig.java
+++ src/main/java/newb/c/api/SpringfoxDocConfig.java
@@ -1,32 +1,22 @@
 package newb.c.api;
 
-import com.google.common.base.Predicate;
 import com.google.common.collect.Sets;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.AuthorizationScopeBuilder;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.BasicAuth;
-import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-
-import static com.google.common.base.Predicates.or;
-import static com.google.common.collect.Lists.newArrayList;
 import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableWebMvc //NOTE: Only needed in a non-springboot application
 @EnableSwagger2 //Enable swagger 2.0 spec
-@ComponentScan("newb.c.controller")
+@ComponentScan("newb.c")
 public class SpringfoxDocConfig {
 	/**
 	 * 访问/swagger-ui.html
