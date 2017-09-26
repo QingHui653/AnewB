e2c7dab0e787a1f3f53b7852ab13d030db16f7c6
DESKTOP-5TMB1MQ\woshizbh <910944453@qq.com>
2017-09-26 11:34:43 +0800

    redis 緩存替换为 redisCacheManager

--- src/main/java/newb/c/controller/TestCacheController.java
+++ src/main/java/newb/c/controller/TestCacheController.java
@@ -6,13 +6,14 @@ import newb.c.backend.service.TestCacheService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 
-//@Controller
+@Controller
 @RequestMapping("cache")
 public class TestCacheController {
 	
