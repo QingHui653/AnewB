edb11b2b212711d762e73619bfae6152332d544b
woshizbh <910944453@qq.com>
2017-11-21 19:00:42 +0800

    spring rabbitmq

--- src/main/java/newb/c/a_spring/api/mq/rabbitmq/RabbitMqListener.java
+++ src/main/java/newb/c/a_spring/api/mq/rabbitmq/RabbitMqListener.java
@@ -1,27 +1,56 @@
 package newb.c.a_spring.api.mq.rabbitmq;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import newb.c.a_spring.backend.sql.model.basemodel.User;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.stereotype.Service;
 
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
 @Service
-public class RabbitMqListener {
-	
+public class RabbitMqListener  {
+
+	@RabbitListener(
+			bindings = {@QueueBinding(
+					value = @Queue(value ="annQueue" ),
+					exchange = @Exchange(value = "annExchange"),
+					key = "annKey"
+			)}
+	)
+	public void annListener(Message msg) throws UnsupportedEncodingException {
+		System.out.println("topic id msg" + msg.getMessageProperties());//消息中的一些参数
+		System.out.println("使用注解的方式 "  + new String(msg.getBody(),"UTF-8"));
+	}
 	@RabbitListener(queues="queue_one")
-	public void queueListener(Object id, Object jobName){
-		System.out.println("Received request for id " + id.toString());
-        System.out.println("Received request for job name " + jobName);
+	public void queueListener(Message msg) throws UnsupportedEncodingException {
+        System.out.println("queue_one msg "  + new String(msg.getBody(),"UTF-8"));
     }
 
 	@RabbitListener(queues="queue_two")
-	public void objectListener(User u, Object jobName){
-		System.out.println("Received request for id " + u.toString());
-		System.out.println("Received request for Total " + jobName);
+	public void objectListener(Message msg) throws UnsupportedEncodingException {
+		System.out.println("queue_two msg "  + new String(msg.getBody(),"UTF-8"));
 	}
 
 	@RabbitListener(queues="queue_three")
-	public void objectListListener(Object l, Object jobName){
-		System.out.println("Received request for id " + l);
-		System.out.println("Received request for Total " + jobName);
+	public void objectListListener(Message msg) throws UnsupportedEncodingException {
+		System.out.println("queue_three msg "  + new String(msg.getBody(),"UTF-8"));
 	}
+
+	@RabbitListener(queues="topic_one")
+	public void topicObjectListListener(Message msg) throws UnsupportedEncodingException {
+		System.out.println("topic id msg" + new String(msg.getBody(),"UTF-8"));
+	}
+
+	@RabbitListener(queues="topic_two")
+	public void topic2ObjectListListener(Message msg) throws UnsupportedEncodingException {
+		System.out.println("topic2 id msg" + new String(msg.getBody(),"UTF-8"));
+	}
+
 }
--- src/main/java/newb/c/a_web/controller/mq/RabbitMqController.java
+++ src/main/java/newb/c/a_web/controller/mq/RabbitMqController.java
@@ -3,6 +3,7 @@ package newb.c.a_web.controller.mq;
 import newb.c.a_spring.backend.sql.model.basemodel.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,35 +13,65 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * JMS|AMQP
+ * Java api(不跨java)|Wire-protocol(跨java)
+ * p2p,pub/sub| direct fanout topic headers  system 五种类型 后四种和JMS的pub/sub模型没有太大差别，仅是在路由机制上做了更详细的划分；
+ */
 
+/**
+ * java 端口 为 5672 web 端口为 15672
+ */
 @Controller
 @RequestMapping("rabbitmq")
 public class  RabbitMqController {
 
     private static final Logger logger = LoggerFactory.getLogger(RabbitMqController.class);
 
-    //ActiveMQ
     @Autowired(required=false)
     private RabbitTemplate rabbitTemplate;
+//    @Autowired(required=false)
+//    private AmqpTemplate rabbitTemplate;
 
-    @GetMapping("string")
+
+    @GetMapping("queue ann")
+    public void testAnnRabbit() {
+        rabbitTemplate.convertAndSend("annExchange","annKey", "注解配置");
+    }
+
+    @GetMapping("queue string")
     public void testRabbit() {
         rabbitTemplate.convertAndSend("queue_one", "hhhhhh");
     }
 
-    @GetMapping("object")
+    @GetMapping("queue object")
     public void testObjectRabbit() {
-        User u =new User(1,"二二","三三");
+        User u =new User(1,"二二","queue_two");
         rabbitTemplate.convertAndSend("queue_two", u);
     }
 
-    @GetMapping("objectList")
+    @GetMapping("queue objectList")
     public void testObjectListRabbit() {
         List l =new ArrayList();
-        User u =new User(1,"二二","三三");
-        User u2 =new User(2,"二二","三三");
+        User u =new User(1,"33","queue_three");
+        User u2 =new User(2,"33","queue_three");
         l.add(u);
         l.add(u2);
         rabbitTemplate.convertAndSend("queue_three", l);
+    }
+
+    @GetMapping("topic one")
+    public void testTopicRabbit() {
+        rabbitTemplate.convertAndSend("topicExchange","topic.one","hhhtopic");
+    }
+
+    @GetMapping("topic objectList")
+    public void testTopicObjectListRabbit() {
+        List l =new ArrayList();
+        User u =new User(1,"二二","topic_three");
+        User u2 =new User(2,"二二","topic_three");
+        l.add(u);
+        l.add(u2);
+        rabbitTemplate.convertAndSend("topicExchange","topic.one.two", l);
     }
 }
