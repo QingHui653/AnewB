31c050e529bb73f5fd195e87f301284f0a27423e
qinghui <910944453@qq.com>
2017-12-03 23:12:08 +0800

    webMagic 添加cookie 爬取

--- src/main/java/newb/c/a_web/controller/WebMagicController.java
+++ src/main/java/newb/c/a_web/controller/WebMagicController.java
@@ -1,17 +1,33 @@
 package newb.c.a_web.controller;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import newb.c.a_spring.api.alipay.util.httpClient.HttpRequest;
+import newb.c.a_spring.backend.sql.model.basemodel.User;
 import newb.c.a_web.controller.component.service.EsSaveMoviePipeline;
 import newb.c.a_web.controller.component.service.MongoDbSaveMoviePipeline;
 import newb.c.a_web.controller.component.webmagic.MovieProcessor;
+import newb.c.util.ExcelUtil;
+import newb.c.util.ExportExcelUtil;
+import org.apache.http.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import io.swagger.annotations.ApiOperation;
 import newb.c.a_web.controller.component.webmagic.GithubRepoPageProcessor;
+import newb.c.a_web.controller.component.webmagic.BearProcessor;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.pipeline.ConsolePipeline;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("webmagic")
@@ -22,6 +38,10 @@ public class WebMagicController {
 	private MongoDbSaveMoviePipeline mongoDbSaveMoviePipeline;
 	@Autowired(required=false)
 	private EsSaveMoviePipeline esSaveMoviePipeline;
+	@Autowired
+	private BearProcessor bearProcessor;
+	@Autowired(required=false)
+	private MongoTemplate mongoTemplate;
 
 	@RequestMapping(value="getGithub",method=RequestMethod.GET)
 	@ApiOperation("测试使用spider爬取github alibaba的项目")
@@ -47,6 +67,43 @@ public class WebMagicController {
         .addPipeline(mongoDbSaveMoviePipeline)
         .thread(50)
         .run();
+	}
+
+	@RequestMapping(value="getBearMovie",method=RequestMethod.GET)
+	public void getBearMovie(){
+		Spider.create(bearProcessor)
+				.addUrl("http://98.126.159.33/V1/Product/detail?pid=1&")
+				.addPipeline(new ConsolePipeline())
+				.thread(5)
+				.run();
+		System.out.println("****************************************************");
+	}
+
+	@RequestMapping(value="getBearMovieExcel",method=RequestMethod.GET)
+	public void getBearMovieExcel(HttpServletResponse response) throws IOException {
+//		Query query = new Query();
+//		query.addCriteria(Criteria.where("data.baidu_link").is(null));
+//		mongoTemplate.remove(query, JSONObject.class);
+//		List<JSONObject> jsonObjectList = mongoTemplate.find(query, JSONObject.class);
+		List<JSONObject> jsonObjectList = mongoTemplate.findAll(JSONObject.class);
+		System.out.println("size  "+jsonObjectList.size());
+		ExportExcelUtil excelUtil = new ExportExcelUtil();
+		String[] headList ={"片名","地址","大小"};
+		excelUtil.simpleExport(response,headList,swap(jsonObjectList));
+	}
+
+	private List<String[]> swap(List<JSONObject> jsonObjectList){
+		List<String[]> list = new ArrayList();
+		for (JSONObject jsonObject : jsonObjectList) {
+			String[] strings =new String[3];
+			JSONObject data = jsonObject.getJSONObject("data");
+			JSONArray tags= data.getJSONArray("tags");
+			strings[0]=tags.toJSONString();
+			strings[1]=data.getString("baidu_link");
+			strings[2]=data.getString("filesize");
+			list.add(strings);
+		}
+		return list;
 	}
 
 	/**
