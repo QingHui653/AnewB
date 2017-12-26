326c66c7dc7ecb406804675eb664afa449caa616
woshizbh <910944453@qq.com>
2017-12-26 19:06:01 +0800

    spring TestApplicationListener Dubbo(ServiceBean)

---
+++ src/main/java/newb/c/a_spring/a_config/springBean/TestApplicationListener.java
@@ -0,0 +1,23 @@
+package newb.c.a_spring.a_config.springBean;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+/**
+ * 那么每当在一个ApplicationEvent发布到 ApplicationContext时，这个bean得到通知
+ *
+ * //http://blog.csdn.net/ilovejava_2010/article/details/7953419
+ */
+@Slf4j
+public class TestApplicationListener implements ApplicationListener {
+    /**
+     * Handle an application event.
+     *
+     * @param event the event to respond to
+     */
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        log.info("进入TestApplicationListener"+event.toString());
+    }
+}
