16f8782718f2e10388e83910f6e6116aefdf073b
qinghui <910944453@qq.com>
2018-01-20 10:55:34 +0800

    fix bug

--- src/main/java/newb/c/a_spring/a_module/mongodb/MongoDBJDBC.java
+++ src/main/java/newb/c/a_spring/a_module/mongodb/MongoDBJDBC.java
@@ -5,6 +5,7 @@
 package newb.c.a_spring.a_module.mongodb;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.mongodb.client.FindIterable;
@@ -51,9 +52,10 @@ public class MongoDBJDBC{
    @Test
    public void creatClientWithPW(){
 	   try {  
-           //连接到MongoDB服务 如果是远程连接可以替换“localhost”为服务器所在IP地址  
+
+          /* //连接到MongoDB服务 如果是远程连接可以替换“localhost”为服务器所在IP地址
            //ServerAddress()两个参数分别为 服务器地址 和 端口  
-           ServerAddress serverAddress = new ServerAddress(url,port);  
+
            List<ServerAddress> addrs = new ArrayList<ServerAddress>();  
            addrs.add(serverAddress);  
              
@@ -66,18 +68,25 @@ public class MongoDBJDBC{
            MongoClient mongoClient = new MongoClient(addrs,credentials);  
              
            //连接到数据库  
-           MongoDatabase mongoDatabase = mongoClient.getDatabase(database);
+           MongoDatabase mongoDatabase = mongoClient.getDatabase(database);*/
 
-
+           ServerAddress serverAddress = new ServerAddress(url,port);
+           //MongoCredential.createScramSha1Credential()三个参数分别为 用户名 数据库名称 密码
+           MongoCredential credential = MongoCredential.createCredential(user, database, password.toCharArray());
+           //通过连接认证获取MongoDB连接
+           MongoClient mongoClient = new MongoClient(serverAddress, Arrays.asList(credential));
+           //连接到数据库
+           MongoDatabase mongoDatabase = mongoClient.getDatabase("upload");
+           System.out.println("Connect to database successfully");
+           long movie = mongoDatabase.getCollection("title").count();
+           System.out.println(movie);
            FindIterable<Document> findIterable = mongoDatabase.getCollection("title").find(Filters.eq("tid", "893321")).sort(orderBy(Sorts.ascending("index")));
 
            MongoCursor<Document> mongoCursor = findIterable.iterator();
            while(mongoCursor.hasNext()){
                System.out.println(mongoCursor.next());
            }
-
-           System.out.println("Connect to database successfully");
-       } catch (Exception e) {  
+       } catch (Exception e) {
            System.err.println( e.getClass().getName() + ": " + e.getMessage() );  
        }  
    }
