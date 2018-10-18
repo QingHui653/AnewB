069aa56601fa68ba9030244f571059849132e39b
qinghui <910944453@qq.com>
2018-10-18 10:00:31 +0800

    fix bug

--- src/test/java/test/core/javacase/leetcode/StringTest.java
+++ src/test/java/test/core/javacase/leetcode/StringTest.java
@@ -322,7 +322,7 @@ public class StringTest {
     /**
      * 编写一个函数来查找字符串数组中的最长公共前缀。
      * 如果不存在公共前缀，返回空字符串 ""。
-     * ["flower","owfl","igflht"]
+     * ["flower","flow","flight"]
      * 输出: "fl"
      *
      * ["dog","racecar","car"]
