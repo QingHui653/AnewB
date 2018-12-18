acbe425df1eb31a80b4141e64868d7167099ceb9
qinghui <910944453@qq.com>
2018-12-18 16:02:30 +0800

    dynamicPlanning [2]

--- src/test/java/test/core/javacase/leetcode/DynamicPlanning.java
+++ src/test/java/test/core/javacase/leetcode/DynamicPlanning.java
@@ -16,8 +16,12 @@ public class DynamicPlanning {
 //        int step =4;
 //        System.out.println("爬"+step+"阶 总共需 "+climbStairs(step));
         //买卖股票
-        int[] prices = {7,2,6,8,1,5,3,8,4};
-        System.out.println("最大利润为"+maxProfit(prices));
+//        int[] prices = {7,2,6,8,1,5,3,8,4};
+//        System.out.println("最大利润为"+maxProfit(prices));
+//        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+//        System.out.println("最大和的连续子数组"+ maxSubArray2(nums));
+        int[] nums = {2,1,1,2};
+        System.out.println(rob(nums));
     }
     /**
      * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
@@ -96,7 +100,83 @@ public class DynamicPlanning {
      * @param nums
      * @return
      */
+    public int maxSubArray2(int[] nums) {
+//        if(nums.length==1) return nums[0];
+//        if(nums.length==2){
+//            if(nums[0]<0 || nums[1]<0)
+//                return nums[0]>nums[1]?nums[0]:nums[1];
+//            return nums[0]+nums[1];
+//        }
+//        int max= nums[0]+nums[1];
+//        int add= nums[0]+nums[1];
+//        for (int i = 1,j=2; i <= nums.length-2 ; i++,j++) {
+////            System.out.println("add "+i+" "+j +" "+(nums[i]+nums[j]));
+//            if(nums[j]>0){
+//                add+=nums[j];
+//                if(i==nums.length-2 && add>max)
+//                    max=add;
+//            }else {
+//                if(max<add){
+//                    max=add;
+//                }
+//                add= nums[i]+nums[j];
+//            }
+//        }
+//        return max;
+        int curMax = nums[0];
+        int maxSum = nums[0];
+        for(int i = 1;i<nums.length;++i){
+            curMax = Math.max(curMax+nums[i],nums[i]);
+            if(curMax > maxSum)
+                maxSum = curMax;
+        }
+
+        return maxSum;
+    }
+
     public int maxSubArray(int[] nums) {
-        return 0;
+        if (nums.length == 1) return nums[0];
+        int sum = 0;
+        int max = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum > max)
+                max = sum;
+            if (sum <= 0) {
+                sum = 0;
+            }
+        }
+        return max;
+    }
+
+    /**
+     * 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+     *
+     * 给定一个代表每个房屋存放金额的非负整数数组，计算你在不触动警报装置的情况下，能够偷窃到的最高金额。
+     *
+     * 示例 1:
+     * 输入: [1,2,3,1]
+     * 输出: 4
+     * 解释: 偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
+     *      偷窃到的最高金额 = 1 + 3 = 4 。
+     * 示例 2:
+     * 输入: [3,7,9,3,1,5]
+     * 输出: 12
+     * 解释: 偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
+     *      偷窃到的最高金额 = 2 + 9 + 1 = 12 。
+     * @param nums
+     * @return
+     */
+    // 分两张情况 奇数,直接 相邻判断即可
+    // 偶数还需要判断1,4这种情况
+    // 2,1,1,2,,2 ->4
+    //https://www.cnblogs.com/grandyang/p/4383632.html
+    public int rob(int[] nums) {
+        int num1=0,num2=0;
+        for (int i = 0; i <nums.length ; i++) {
+            if( i % 2==0) num1=Math.max(num1+nums[i],num2);
+            else num2=Math.max(num1,num2+nums[i]);
+        }
+        return num1>num2?num1:num2;
     }
 }
